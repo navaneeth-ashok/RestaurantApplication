9c6ec168a51714617fc6e87c16efa1d1c4d6dab0
navaneeth_ashok_1 <navaneeth619@gmail.com>
2021-06-12 23:02:05 +0530

    Added server calculated bill amount in the order detail page, added viewmodel to receive all info regarding the order

---
+++ RestaurantApplication/Models/ViewModel/OrderStatusFoodDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantApplication.Models.ViewModel
+{
+    public class OrderStatusFoodDetail
+    {
+        public OrderID OrderIDClassDetails { get; set; }
+        public List<OrderItem> OrderItemDetails { get; set; }
+        public decimal TotalOrderAmount { get; set; }
+    }
+}
